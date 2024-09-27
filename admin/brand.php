b3293c3b91814e050c2b8d911aa6a84a53895cc2
kevin <kevinthulnith@gmail.com>
2024-09-28 00:32:09 +0530

    admin interfacee

---
+++ admin/brand.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Zee Store - Brand</title>
+  <link
+    rel="icon"
+    type="image/x-icon"
+    href="../svg/logo2.png"
+    media="(prefers-color-scheme: light)" />
+  <link
+    rel="icon"
+    type="image/x-icon"
+    href="../svg/logo1.png"
+    media="(prefers-color-scheme: dark)" />
+  <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="stylesheet" href="../css/open-iconic-bootstrap.min.css" />
+  <link rel="stylesheet" href="../css/owl.carousel.min.css" />
+  <link rel="stylesheet" href="../css/owl.theme.default.min.css" />
+  <link rel="stylesheet" href="../css/magnific-popup.css" />
+  <link rel="stylesheet" href="../css/aos.css" />
+  <link rel="stylesheet" href="../css/ionicons.min.css" />
+  <link rel="stylesheet" href="../css/animate.css" />
+  <link rel="stylesheet" href="../css/bootstrap-datepicker.css" />
+  <link rel="stylesheet" href="../css/jquery.timepicker.css" />
+  <link rel="stylesheet" href="../css/flaticon.css" />
+  <link rel="stylesheet" href="../css/icomoon.css" />
+  <link rel="stylesheet" href="../css/style.css" />
+  <link rel="stylesheet" href="../css/style2.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+    integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer" />
+</head>
+
+<body style="margin-top: -30px">
+  <nav>
+    <a href="index.html" class="brand">Zee Store</a>
+    <div>
+      <ul id="navbar">
+        <li><a href="dashboard.php">Dashboard</a></li>
+        <li><a href="brand.php" class="active">Brand</a></li>
+        <li><a href="catagory.php">Catagory</a></li>
+        <li><a href="supplier.php">Supplier</a></li>
+        <li><a href="product.php">Product</a></li>
+        <li class="user" id="user">
+          <div class="circle"></div>
+          <i class="fa fa-user"></i>
+        </li>
+        <a href="#" id="close"><i class="far fa-times"></i></a>
+      </ul>
+      <div id="userbar">
+        <li><a href="singup1.html">Settings</a></li>
+        <li><a href="login.html">Logout</a></li>
+        <a href="#" id="asd"><i class="fa-solid fa-xmark"></i></a>
+      </div>
+    </div>
+    <div class="show">
+      <div class="user2" id="user2">
+        <div class="circle"></div>
+        <i class="fa fa-user"></i>
+        <i class="fa fa-exclamation-circle"></i>
+      </div>
+      <i id="bar" class="fas fa-outdent"></i>
+    </div>
+  </nav>
+  <!-- END nav -->
+
+  <?php
+  require_once '../php/DbConnect.php';
+  $ghr = "";
+
+  // retreve data if form is submitted
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Get image file info
+    $image = $_FILES['image']['tmp_name'];
+    $iimage_type = $_FILES['image']['type'];
+    $image_name = $_FILES['image']['name'];
+
+    // Read the image as a binary string
+    $image_data = addslashes(file_get_contents($image));
+
+    // input data to db
+    $sql = "INSERT INTO `brand`(`brand_name`,`image`, `image_type`) VALUES ('" . $_POST['name'] . "','$image_data', '$iimage_type')";
+    $result1 = $conn->query("SELECT * FROM brand WHERE brand_name = '" . $_POST['name'] . "' ;");
+    if ($result1->num_rows > 0)  $ghr = "Brand already exists";
+    elseif ($conn->query($sql)) {
+      $ghr = "Process successfully done";
+    } else $ghr = "Invalid data detected";
+  }
+  ?>
+
+  <div class="popup <?php if ($ghr == "") echo "hide"; ?>">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+      <span>Inventory Managment System</span>
+      <h2>Add new brand</h2>
+      <div class="xmark"><i class="fa-solid fa-xmark"></i></div>
+      <div class="form-body">
+        <div class="image"><img src="../svg/image.svg" class="display" /></div>
+        <div class="fields">
+          <div class="message <?php if ($ghr == "") echo "pop"; ?>">
+            <i class="fa-solid fa-circle"></i>
+            <?php echo $ghr ?>
+          </div>
+          <div class="inputs">
+            <label>Name</label>
+            <input type="text" name="name" id="" placeholder="name..." required />
+          </div>
+          <div class="inputs">
+            <label>Logo</label>
+            <input
+              type="file"
+              name="image"
+              id="image_input"
+              accept=".jpg, .jpeg, .png, .img"
+              title="edit properly before uploading"
+              required />
+          </div>
+          <button type="submit">Submit</button>
+        </div>
+      </div>
+    </form>
+    <?php if (isset($error)) echo "<p>$error</p>" ?>
+  </div>
+  <!-- popup -->
+
+  <section
+    class="hero-wrap hero-wrap-2"
+    style="background-image: url('../images/shopping.jpg')"
+    data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+      <div
+        class="row no-gutters slider-text align-items-center justify-content-center">
+        <div class="col-md-9 ftco-animate text-center">
+          <h1 class="mb-2 bread">Manage Brands</h1>
+          <p class="breadcrumbs">
+            <span class="mr-2">
+              <a href="index.html">
+                Dashboard
+                <i class="ion-ios-arrow-forward"></i>
+              </a>
+            </span>
+            <span>
+              Brands
+              <i class="ion-ios-arrow-forward"></i>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- header -->
+
+  <section class="admin-table">
+    <h2>Product brand table</h2>
+    <span>view and manage product brand information</span>
+    <div class="table">
+      <div class="searchAddS">
+        <section class="search">
+          <input type="text" id="search" placeholder="search brand..." />
+          <button id="search-bt">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </section>
+        <button id="btt1">Add Brand</button>
+      </div>
+      <div class="table-header parent">
+        <div class="table-header-data row">Brand ID</div>
+        <div class="table-header-data row">Name</div>
+        <div class="table-header-data row">NO_Products</div>
+        <div class="table-header-data row">Status</div>
+      </div>
+      <div class="table-data">
+        <?php
+        require_once '../php/DbConnect.php';
+        $result = $conn->query("SELECT * FROM `brand` WHERE 1; ");
+        if ($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) {
+            echo "<div class='table-row parent'>";
+            echo "<div class='table-cell row'>" . $row['brand_id'] . "</div>";
+            echo "<div class='table-cell row'>";
+            echo $row['brand_name'];
+            echo "<div class='logo'>";
+            echo '<img src="data:' . $row['image_type'] . ';base64,' . base64_encode($row['image']) . '" alt="Image" />';
+            echo "</div>";
+            echo "</div>";
+            echo "<div class='table-cell row'>" . 20 . "</div>";
+            echo "<div class='table-cell row'>";
+            if ($row['brand_status']) echo "<a href='disable.php?id=" . $row['brand_id'] . "'> Active<i class='fa-solid fa-circle-check' style='margin-left:5px'></i></a>";
+            else echo "<a href='disable.php?id=" . $row['brand_id'] . "'> Disabled </a>";
+            echo "</div>";
+            echo "</div>";
+          }
+        } else echo "<div class='table-row parent'> No records found </div>";
+        ?>
+      </div>
+    </div>
+  </section>
+
+  <div id="ftco-loader" class="show fullscreen">
+    <.. /svg class="circular" width="48px" height="48px">
+      <circle
+        class="path-bg"
+        cx="24"
+        cy="24"
+        r="22"
+        fill="none"
+        stroke-width="4"
+        stroke="#eeeeee" />
+      <circle
+        class="path"
+        cx="24"
+        cy="24"
+        r="22"
+        fill="none"
+        stroke-width="4"
+        stroke-miterlimit="10"
+        stroke="#F96D00" />
+    </.. /svg>
+  </div>
+  <!-- loader -->
+
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/jquery.easing.1.3.js"></script>
+  <script src="../js/jquery.waypoints.min.js"></script>
+  <script src="../js/jquery.stellar.min.js"></script>
+  <script src="../js/owl.carousel.min.js"></script>
+  <script src="../js/jquery.magnific-popup.min.js"></script>
+  <script src="../js/aos.js"></script>
+  <script src="../js/jquery.animateNumber.min.js"></script>
+  <script src="../js/bootstrap-datepicker.js"></script>
+  <script src="../js/jquery.timepicker.min.js"></script>
+  <script src="../js/scrollax.min.js"></script>
+  <script src="../https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="../js/google-map.js"></script>
+  <script src="../js/main.js"></script>
+  <script src="../js/home.js"></script>
+</body>
+
+</html>
