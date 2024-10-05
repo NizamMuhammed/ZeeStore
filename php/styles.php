92a20963a7b64301c3537fb5fe40a29e69bf8c8f
NizamMuhammed <muhammedniz97@gmail.com>
2024-10-05 22:38:44 +0530

    changes

--- php/styles.php
+++ php/styles.php
@@ -31,3 +31,16 @@
   integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
   crossorigin="anonymous"
   referrerpolicy="no-referrer" />
+
+<?php
+function navigation(int $n)
+{
+  echo ($n == 1) ? "<li><a class='active' href='dashboard.php'>Dashboard</a></li>" : "<li><a href='dashboard.php' >Dashboard</a></li>";
+  echo ($n == 2) ? "<li><a class='active' href='brand.php'>Brands</a></li>" : "<li><a href='brand.php'>Brands</a></li>";
+  echo ($n == 3) ? "<li><a class='active' href='catagory.php'>Catagory</a></li>" : "<li><a href='catagory.php'>Catagory</a></li>";
+  echo ($n == 4) ? "<li><a class='active' href='supplier.php'>Suppliers</a></li>" : "<li><a href='supplier.php'>Suppliers</a></li>";
+  echo ($n == 5) ? "<li><a class='active' href='product.php'>Product</a></li>" : "<li><a href='product.php'>Product</a></li>";
+  echo ($n == 6) ? "<li><a class='active' href='orders.php'>Orders</a></li>" : "<li><a href='orders.php'>Orders</a></li>";
+  echo ($n == 7) ? "<li><a class='active' href='payments.php'>Payments</a></li>" : "<li><a href='payments.php'>Payments</a></li>";
+}
+?>
